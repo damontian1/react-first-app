0053aaed0bcbba1bcd3ad57053024a43b79417ab
Damon Tian <damontian@live.com>
2016-12-02 18:00:34 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.render(
-  <App name="d a m o n"/>,
-  document.getElementById('root')
-);
