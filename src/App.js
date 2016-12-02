0b78bb71758e3af5acc52091d7e3f8a530189368
Damon Tian <damontian@live.com>
2016-12-02 17:51:18 -0500

    sanity test 1

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+
+var App = React.createClass({
+  render: function(){
+    return(
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>DAMON!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hi my name is {this.props.name}
         </p>
       </div>
-    );
+    )
   }
-}
+})
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App name="d a m o n"/>,
   document.getElementById('root')
 );
